c0e3c5dcc99fdf908a029df5ad5cd11fd3655c91
PapSara <114569211+PapSara@users.noreply.github.com>
2025-05-27 12:37:02 +0300

    final AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-using MFAAuthApp.Models; // adaugă asta!!
+using MFAAuthApp.Models;
 
 public class AppDbContext : DbContext
 {
     public DbSet<User> Users => Set<User>();
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configurări pentru entitatea User
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Username).HasMaxLength(100).IsRequired();
+            entity.Property(e => e.PasswordHash).IsRequired();
+            entity.HasIndex(e => e.Username).IsUnique();
+        });
+    }
 }
